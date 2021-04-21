cd63d44681aee5c4e9efed84e00a36770b8f33b2
Joz-Bruer Quince <quincejozbruer@gmail.com>
2021-04-20 21:57:16 -0700

    Connection to the database

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,39 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
-
-
-const app = express()
-
-app.use(bodyParser.json())
+const mongoose = require('mongoose')
 
 const placesRoutes = require('./routes/places-routes')
 const usersRoutes = require('./routes/users-routes')
 
 const HttpError = require('./models/http-error')
 
-app.use('/api/places',placesRoutes)
+mongoose
+	.connect(
+		'mongodb+srv://joz:@mern.aoodt.mongodb.net/MernData?retryWrites=true&w=majority',
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	)
+	.then(() => console.log('Connection to MongoDB success'))
+	.catch(() => console.log('Connection Failed'))
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use('/api/places', placesRoutes)
 app.use('/api/users', usersRoutes)
 
-app.use((request, response, next) =>{
-    const error = new HttpError('We cannot find this route', 404)
-    return next(error)
+app.use((request, response, next) => {
+	const error = new HttpError('We cannot find this route', 404)
+	return next(error)
 })
 
-app.use((error, request, response, next ) => {
-    if (response.headerSent){
-        return next(error)
-    }
-    response.status(error.code || 500)
-    response.json({Messages: error.message || 'An unknown error occured'})
+app.use((error, request, response, next) => {
+	if (response.headerSent) {
+		return next(error)
+	}
+	response.status(error.code || 500)
+	response.json({ Messages: error.message || 'An unknown error occured' })
 })
 module.exports = app
