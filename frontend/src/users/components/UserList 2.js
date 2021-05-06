277349d9d0b5a0f914cb69d6d7e4340247de25ed
Joz-Bruer Quince <quincejozbruer@gmail.com>
2021-05-06 16:59:12 -0700

    Update

---
+++ frontend/src/users/components/UserList 2.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Card from '../../shared/components/UIelements/Card'
+import UserItem from './UserItem'
+import './UserList.css'
+
+function UserList(props) {
+    return (
+        <>
+        {props.items.length === 0 ? (
+            <div className='users-list center'>
+                <Card>
+                    No user found
+                </Card>
+            </div>
+        ) : (
+            <ul className='users-list'>
+                {props.items.map(user =>{
+                    return <UserItem
+                        key={user.id}
+                        id={user.id}
+                        image={user.image}
+                        name={user.name}
+                        placeCount={user.places}
+                    />
+                })}
+            </ul>
+        )}
+        </>
+    )
+}
+
+export default UserList
